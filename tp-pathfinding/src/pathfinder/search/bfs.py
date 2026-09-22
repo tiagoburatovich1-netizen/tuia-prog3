d1f576fc82dcc0a9b878eb09b415907730a5973a
Tiago Buratovich <tiagoburatovich1@gmail.com>
2026-09-22 13:02:48 -0300

    Implementacion de BFS

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -23,7 +23,37 @@ class BreadthFirstSearch:
         reached[root.state] = True
 
         # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        frontier = QueueFrontier()
+        frontier.add(root)
 
+        while not frontier.is_empty():
+            node = frontier.remove()
+
+            #test objetivo
+            if grid.objective_test(node.state):
+                return Solution(node, reached)
+
+            #explora las posibles acciones
+            for action in grid.actions(node.state):
+                successor_state = grid.result(node.state, action)
+
+                #omite a los estados ya alcanzados
+                if successor_state in reached:
+                    continue
+                #marca el sucesor como alcanzado
+                reached[successor_state] = True
+
+                #crea el nodo hijo
+                son = Node(
+                    "",
+                    state=successor_state,
+                    cost=node.cost + grid.individual_cost(node.state, action),
+                    parent=node,
+                    action=action,
+                )
+
+                #agrega el hijo a la frontera
+                frontier.add(son)
+
+        #no se encontro solucion
         return NoSolution(reached)
