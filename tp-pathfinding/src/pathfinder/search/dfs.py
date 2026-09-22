2066397c7c1e8485f2737e774bcd5c33f7326f73
Tiago Buratovich <tiagoburatovich1@gmail.com>
2026-09-22 14:03:40 -0300

    implementacion de DFS

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -21,8 +21,41 @@ class DepthFirstSearch:
         # Initialize expanded with the empty dictionary
         expanded = dict()
 
+        # test objetivo
+        if grid.objective_test(root.state):
+            return Solution(root, expanded)
+        
         # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        frontier = StackFrontier()
+        frontier.add(root)
 
+        while not frontier.is_empty():
+            # Extrae el próximo nodo de la pila
+            node = frontier.remove()
+
+            # Si el estado aún no ha sido expandido
+            if node.state in expanded:
+                continue
+            expanded[node.state] = True
+            
+            for action in grid.actions(node.state):
+                successor_state = grid.result(node.state, action)
+
+                # omite a los estados ya alcanzados
+                if successor_state not in expanded:
+                    # crea el nodo hijo
+                    son = Node(
+                        "",
+                        successor_state,
+                        cost=node.cost + grid.individual_cost(node.state, action),
+                        parent=node,
+                        action=action,
+                    )
+                    # test objetivo
+                    if grid.objective_test(successor_state):
+                        return Solution(son, expanded)
+
+                    # agrega el hijo a la frontera
+                    frontier.add(son)
+                
         return NoSolution(expanded)
